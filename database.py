c0e4112607f9394862fcea4ee8508994c864d7a8
Nico-Lop <nicolas.lopez2711@gmail.com>
2023-09-28 12:52:15 -0400

    Created a database and added a table

---
+++ database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect('people.db')
+
+# Create a cursor
+cursor = conn.cursor()
+
+# cretate a table
+cursor.execute("""CREATE TABLE people (
+               first_name text, 
+               last_name text, 
+               DOB text,
+               SSN text,
+            )""")
+
+# Datatypes:
+# NULL
+# INTEGER
+# REAL
+# TEXT
+# BLOB
+
+# Commit our command
+conn.commit()
+
+# Close our connection
+conn.close()
